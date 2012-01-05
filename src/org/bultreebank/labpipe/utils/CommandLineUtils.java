0ca69a04e2ff1b8a2c95f24b65128929141063dd
Aleksandar Savkov <cytehuop@gmail.com>
2012-01-05 09:20:53 +0200

    fixed command line validation printing

--- src/org/bultreebank/labpipe/utils/CommandLineUtils.java
+++ src/org/bultreebank/labpipe/utils/CommandLineUtils.java
@@ -316,7 +316,6 @@ public class CommandLineUtils {
      */
     public static boolean validateParameters(String[] args) {
 
-        System.out.println(args.length);
         if (args.length == 0) {
             System.out.println(HELP_MESSAGE);
             return false;
@@ -328,7 +327,6 @@ public class CommandLineUtils {
         }
 
         for (int i = 0; i < args.length; i++) {
-            System.out.println();
             if (!PARAMETERS.contains(args[i])) {
                 throwCommandLineError(ERROR_INVALID_PARAMETER, args[i]);
             }
